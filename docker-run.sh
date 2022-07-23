1b3b14ceee4e05aa69c42dc60ae271cc2a671c58
Favio Manriquez <favio@favrik.com>
2022-07-23 11:52:09 -0700

    Fix hasura version

--- docker-run.sh
+++ docker-run.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
-docker run -d --net=host \
+docker stop da8
+docker rm da8
+docker run -d --name da8 --net=host \
        -e HASURA_GRAPHQL_DATABASE_URL=postgres://postgres:postgres@localhost:5432/hasurada8 \
        -e HASURA_GRAPHQL_ENABLE_CONSOLE=true \
-       hasura/graphql-engine:latest
+       hasura/graphql-engine:v2.8.4
